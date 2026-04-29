8649a6f8d5e280acbf23244a0bd2ff6d8184be20
Kat <Katlego.Kungoane@bbd.co.za>
2026-04-29 04:21:59 +0200

    potential fox 2

--- GeneticAlgorithm/FightingIceProblem.py
+++ GeneticAlgorithm/FightingIceProblem.py
@@ -86,7 +86,8 @@ def evaluate_individual(x: np.ndarray, settings: IndividualSettings) -> list[flo
 
     competitive_balance: float = f.transform_win_rate(average_win_rate)
 
-    excitement = asyncio.run(gf.calculate_excitement(settings.experiment_name))
+    tmp_experiment_name = f'{settings.experiment_name}_iter_{settings.experiment_suffix}'
+    excitement = asyncio.run(gf.calculate_excitement(tmp_experiment_name))
 
     # return np.array([-uniqueness_reward, -competitive_balance], dtype=np.float64)
     return np.array([-excitement, 0], dtype=np.float64)
