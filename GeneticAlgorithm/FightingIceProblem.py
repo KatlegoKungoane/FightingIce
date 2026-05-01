95da5353f1edb2fdd50b9b060f7bf371b9fd1df4
Kat <Katlego.Kungoane@bbd.co.za>
2026-05-01 08:30:36 +0200

    already 1-, use this

--- GeneticAlgorithm/FightingIceProblem.py
+++ GeneticAlgorithm/FightingIceProblem.py
@@ -87,7 +87,7 @@ def evaluate_individual(x: np.ndarray, settings: IndividualSettings) -> list[flo
     competitive_balance: float = f.transform_win_rate(average_win_rate)
 
     tmp_experiment_name = f'{settings.experiment_name}_iter_{experiment_suffix_time}_{experiment_suffix_uuid}'
-    excitement = 1 - asyncio.run(gf.calculate_excitement(tmp_experiment_name))
+    excitement = asyncio.run(gf.calculate_excitement(tmp_experiment_name))
 
     # return np.array([-uniqueness_reward, -competitive_balance], dtype=np.float64)
     return np.array([-excitement, 0], dtype=np.float64)
